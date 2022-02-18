28d4da709c6c3e678f89723a64a96f0fd07e9c46
Sathyapriyan P V <49948911+pvsathyapriyan@users.noreply.github.com>
2022-02-18 23:07:36 +0530

    Added allowed hosts

--- abisilksarees/settings.py
+++ abisilksarees/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'ih%+8vem)#9%3kdo6beg)!&o%(d%bkbeta5%n)t_!%ye85x4l#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.herokuapp.com']
 
 
 # Application definition
